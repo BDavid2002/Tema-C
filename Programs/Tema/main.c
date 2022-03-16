ca2e2565a21ba59acf24287157a4c4d7f18b9f89
BDavid2002 <47551805+BDavid2002@users.noreply.github.com>
2022-03-16 02:13:50 -0700

    Add files via upload

---
+++ Programs/Tema/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("\n-------CV-------");
+    printf("\nNumele de familie: ");
+    scanf("%d",a);
+    printf("\nPrenume: ");
+    scanf("%d",b);
+    printf("\nData nasterii: ");
+    scanf("%d",c);
+    printf("\nExperienta: ");
+    scanf("%d",d);
+
+    return 0;
+}
+
+
+#include<stdio.h>
+void main()
+{
+	printf("\n NAME:- Rajnish K.");
+	printf("\n ADDRESS:- House No 162, Sector 14, Allahabad, U.P, India");
+	printf("\n EDUCATION:- Pursuing Bachelor of Computer Application");
+	printf("\n MOBILE:- +91 8858XXXXXX \n\n");
+}
---
+++ Programs/Tema1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void main(void)
+{
+    printf("Hello\n");
+    return 0;
+}
